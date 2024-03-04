77305f51050e9e71cac2499299a93d5c80924dfd
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-03-04 16:42:53 +0100

    Project Initialization

---
+++ src/test/java/com/dgmf/SpringSecurity6ApplicationSpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity6ApplicationSpringBoot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
